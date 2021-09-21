2df4d560c9ee0ebecd2dfba136f2bd302fdc7dbd
LnX <luan38.eric@gmail.com>
2021-09-21 20:56:16 -0300

    Finalizando o mobile

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let btnMenu = document.getElementById('btnMenu');
+let menu = document.getElementById('menu');
 
+let openMenu = false;
 
 function changeMenu(){
-    const container = document.getElementById('container')
-    container.classList.toggle('active')
+   // const container = document.getElementById('container')
+    menu.classList.toggle('active')
+    
+    openMenu = !openMenu
 
+    if (openMenu ) menu.style.display = 'flex';
+    else menu.style.display = 'none';
 } 
 
 btnMenu.addEventListener('click', changeMenu);
